362841d5f4e71742565dfcef63ba06cfb3745325
DNN <hstsnam135@gmail.com>
2024-10-10 07:22:40 +0700

    add and train conf 8

--- visualize.py
+++ visualize.py
@@ -2,8 +2,10 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-config_path = "conf_6_new"
-num_epochs = 40
+#############################################
+config_path = "conf_7_new"
+num_epochs = 22
+#############################################
 
 def load_training_log(config_folder_dir, num_epochs):
     log_dir = os.path.join(config_folder_dir, f"running/")
@@ -47,7 +49,6 @@ def plot_metrics(train_losses, test_losses, test_accuracies, test_wers, config_f
     plt.ylabel('Loss') 
     plt.title('Training Loss')
     plt.legend()
-    # plt.subplot(2, 2, 1).set_xlim([0, last_epoch])
     plt.grid(linestyle='--', alpha=0.7)
 
     plt.subplot(2, 2, 2).set_xlim([0, last_epoch])
@@ -56,7 +57,6 @@ def plot_metrics(train_losses, test_losses, test_accuracies, test_wers, config_f
     plt.ylabel('Loss')
     plt.title('Test Loss')
     plt.legend()
-    # plt.set_xlim([0, last_epoch])
     plt.grid(linestyle='--', alpha=0.7)
 
     plt.subplot(2, 2, 3).set_xlim([0, last_epoch])
@@ -65,7 +65,6 @@ def plot_metrics(train_losses, test_losses, test_accuracies, test_wers, config_f
     plt.ylabel('Accuracy')
     plt.title('Test Accuracy')
     plt.legend()
-    # plt.set_xlim([0, last_epoch])
     plt.grid(linestyle='--', alpha=0.7)
     
     plt.subplot(2, 2, 4).set_xlim([0, last_epoch])
@@ -74,12 +73,10 @@ def plot_metrics(train_losses, test_losses, test_accuracies, test_wers, config_f
     plt.ylabel('WER')
     plt.title('Test WER')
     plt.legend()
-    # plt.set_xlim([0, last_epoch])
     plt.grid(linestyle='--', alpha=0.7)
     
     save_dir = os.path.join(config_folder_dir, f"running/")
     plt.tight_layout()
-    # plt.savefig(os.path.join(save_dir, f'training_metrics_{last_epoch}.svg'))
     plt.savefig(os.path.join(save_dir, f'training_metrics_{last_epoch}.pdf'))
     plt.show()
 
